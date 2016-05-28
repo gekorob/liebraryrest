b2a91ca53be9de923ff7710541a15fe8c2970dd0
Roberto Ciatti <roberto.ciatti@gmail.com>
2016-05-28 18:12:09 +0200

    db session commit in order to set id

--- tests/repositories/test_author_repository.py
+++ tests/repositories/test_author_repository.py
@@ -11,6 +11,7 @@ def test_list_empty_authors(db):
 
 def test_list_authors(db):
     author_list = AuthorFactory.create_batch(3)
+    db.session.commit()
     repo = AuthorRepository()
 
     expected_authors = [do.Author(a.id, a.name, a.birth_date) for a in author_list]
@@ -21,6 +22,8 @@ def test_list_authors(db):
 def test_list_authors_using_filter_on_name(db):
     AuthorFactory.create_batch(3)
     auth = AuthorFactory(first_name='Asimov')
+    db.session.commit()
+
     repo = AuthorRepository()
 
     expected_authors = [do.Author(auth.id, auth.name, auth.birth_date)]
