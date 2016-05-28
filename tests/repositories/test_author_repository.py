a9d4d1ec5d9167d7e13bdb06a93a122000c077f1
Roberto Ciatti <roberto.ciatti@gmail.com>
2016-05-28 18:07:35 +0200

    sqlalchemy session commit for id set

--- tests/repositories/test_author_repository.py
+++ tests/repositories/test_author_repository.py
@@ -11,6 +11,7 @@ def test_list_empty_authors(db):
 
 def test_list_authors(db):
     author_list = AuthorFactory.create_batch(3)
+    db.session.commit()
     repo = AuthorRepository()
 
     expected_authors = [do.Author(a.id, a.name, a.birth_date) for a in author_list]
